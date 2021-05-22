e4a5f895ea68f2771cb499b15a6a413eea620dbf
Artem Reiter <reyter.artiom1@gmail.com>
2021-05-22 10:49:31 +0300

    second attempt to pass hw12

---
+++ src/main/java/Main.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.Phaser;
+
+public class Main {
+    Phaser PHASER = new Phaser(4);
+    static String INPUUT = "HHHHHHHHHHHHOOOOOOHHHHHHOO";
+
+    public static void main(String[] args) {
+        new Main().makeWater();
+
+    }
+
+    public void makeWater() {
+        int phase = 0;
+
+        for (; phase < INPUUT.length() / 3; phase++) {
+            new Hydrogen(PHASER);
+            new Hydrogen(PHASER);
+            new Oxygen(PHASER);
+            PHASER.arriveAndAwaitAdvance();
+        }
+    }
+
+    static class Oxygen extends Thread {
+        Phaser phaser;
+
+        public Oxygen(Phaser phaser) {
+            this.phaser = phaser;
+            start();
+        }
+        @Override
+        public void run() {
+            System.out.print("O");
+            phaser.arriveAndAwaitAdvance();
+        }
+    }
+
+    static class Hydrogen extends Thread {
+        Phaser phaser;
+
+        public Hydrogen(Phaser phaser) {
+            this.phaser = phaser;
+            start();
+        }
+
+        @Override
+        public void run() {
+            System.out.print("H");
+            phaser.arriveAndAwaitAdvance();
+        }
+    }
+}
